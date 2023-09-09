9d90f3e5679011bac850726b921968cf4cd83d56
Natalia <amelina.11@mail.ru>
2023-09-10 00:20:42 +0300

    "добавила 7 задание"

---
+++ Sem1Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿// / Задание №7
+
+int num = int.Parse(Console.ReadLine()??"0");
+if(num>100&&num<1000)
+{
+    int lastDigit=num%10;
+    Console.WriteLine(" Последняя цифра числа " +num+ " это " + lastDigit);
+}
+else
+Console.WriteLine("Число не трёхзначное");
