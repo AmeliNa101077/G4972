eb88c563b4621809027ef18620f4b7ea0e72dcc0
Natalia <amelina.11@mail.ru>
2023-09-09 22:37:06 +0300

    "добавила задание 1"

---
+++ Sem1Task1/Program.cs
@@ -0,0 +1,6 @@
+﻿// Задание №1
+Console.WriteLine("Введите число 1:");
+int num1 = int.Parse(Console.ReadLine()??"");
+Console.WriteLine("Введите число 2:");
+int num2 = int.Parse(Console.ReadLine()??"");
+Console.WriteLine(num2==Math.Pow(num1,2)?"Да":"Нет");
