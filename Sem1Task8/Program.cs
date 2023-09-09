320a70253f5ecf52fef68d4d997bbdf9eb6b00df
Natalia <amelina.11@mail.ru>
2023-09-10 01:04:43 +0300

    добавила 8 задание

---
+++ Sem1Task8/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задание №8
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+Console.Write("Введите число N:");
+int numN = int.Parse(Console.ReadLine()??"");
+int even = 2;
+
+while (numN >= even)
+{
+    Console.Write(even + " ");
+    even = even + 2;
+}
