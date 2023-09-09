698b7beb14bb6939f70091906e622a8e899e53c3
Natalia <amelina.11@mail.ru>
2023-09-09 23:25:07 +0300

    "добавила 3 задание"

---
+++ Sem1Task3/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задание №3
+string day=Console.ReadLine()??"0";
+
+switch(day) 
+{ 
+        case "1" : Console.WriteLine("Понедельник") ; break;
+        case "2" : Console.WriteLine("Вторник") ; break;
+        case "3" : Console.WriteLine("Среда") ; break;
+        case "4" : Console.WriteLine("Четверг") ; break;
+        case "5" : Console.WriteLine("Пятница") ; break;
+        case "6" : Console.WriteLine("Суббота") ; break;
+        case "7" : Console.WriteLine("Воскресение") ; break;
+        default : Console.WriteLine("Неверный ввод") ; break;
+}
+
+
