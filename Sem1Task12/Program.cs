e45e21705b2724821dea9e72b79ada92d6a37fc5
Natalia <amelina.11@mail.ru>
2023-09-10 11:04:10 +0300

    Добавила задание 12

---
+++ Sem1Task12/Program.cs
@@ -0,0 +1,40 @@
+﻿//Задание № 12
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+//Вводим число
+Console.WriteLine("Введите число :");
+int num = int.Parse(Console.ReadLine()??"");
+// Находим третью цифру числа.
+int thirdDigit = GetThirdDigit(num);
+if (thirdDigit != -1)
+{
+    Console.WriteLine("Третья цифра числа: " + thirdDigit);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+Console.ReadLine();
+static int GetThirdDigit(int num)
+{
+    int digitCount = GetDigitCount(num);
+    if (digitCount >= 3)
+    {
+        int divider = (int)Math.Pow(10, digitCount - 3);
+        return (num / divider) % 10;
+    }
+    return -1;
+}
+static int GetDigitCount(int num)
+{
+    int count = 0;
+    int temp = num;
+    while (temp != 0)
+    {
+        temp /= 10;
+        count++;
+    }
+    return count;
+}
