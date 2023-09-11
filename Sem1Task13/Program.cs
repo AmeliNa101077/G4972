c450dc5537ab0adddd713728520eba7db61c5dbc
Natalia <amelina.11@mail.ru>
2023-09-11 22:21:31 +0300

    добавлено задание 13

---
+++ Sem1Task13/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задание № 13
+Console.WriteLine("Введите первое число:");
+int numA = int.Parse(Console.ReadLine()??"0");
+
+Console.WriteLine("Введите второе число:");
+int numB = int.Parse(Console.ReadLine()??"0");
+
+ Console.WriteLine(numB%numA==0?"Второе число кратно первому":"остаток от деления:"+(numB%numA));
