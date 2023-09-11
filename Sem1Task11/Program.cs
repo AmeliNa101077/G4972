d5e0064a82cf5331b9cd28e6ddd8ff5c16bd798a
Natalia <amelina.11@mail.ru>
2023-09-11 21:56:53 +0300

    Добавлена 11 задача

---
+++ Sem1Task11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задание 11
+// Введите трехзначное число и удалите из него вторую цифру.
+int num = new Random().Next(100,1000);
+Console.WriteLine(num);
+int firstDigit=num/100;
+int lastDigit = num%10;
+num = firstDigit*10+lastDigit;
+Console.WriteLine(num);
