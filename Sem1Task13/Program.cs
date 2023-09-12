326a5f00639a4b2dfefcb5fe1fa0866594354c81
Natalia <amelina.11@mail.ru>
2023-09-12 21:23:41 +0300

    дополнила решения

--- Sem1Task13/Program.cs
+++ Sem1Task13/Program.cs
@@ -1,8 +1,40 @@
-﻿// Задание № 13
-Console.WriteLine("Введите первое число:");
-int numA = int.Parse(Console.ReadLine()??"0");
-
-Console.WriteLine("Введите второе число:");
-int numB = int.Parse(Console.ReadLine()??"0");
-
- Console.WriteLine(numB%numA==0?"Второе число кратно первому":"остаток от деления:"+(numB%numA));
+﻿//Задание № 13
+// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+//645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+//Вводим число
+Console.WriteLine("Введите число :");
+int num = int.Parse(Console.ReadLine()??"");
+// Находим третью цифру числа.
+int thirdDigit = GetThirdDigit(num);
+if (thirdDigit != -1)
+{
+    Console.WriteLine("Третья цифра числа: " + thirdDigit);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+Console.ReadLine();
+static int GetThirdDigit(int num)
+{
+    int digitCount = GetDigitCount(num);
+    if (digitCount >= 3)
+    {
+        int divider = (int)Math.Pow(10, digitCount - 3);
+        return (num / divider) % 10;
+    }
+    return -1;
+}
+static int GetDigitCount(int num)
+{
+    int count = 0;
+    int temp = num;
+    while (temp != 0)
+    {
+        temp /= 10;
+        count++;
+    }
+    return count;
+}
