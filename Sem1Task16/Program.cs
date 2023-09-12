68b465f9d9c46383abac09eecd827d5a983849dc
Natalia <amelina.11@mail.ru>
2023-09-12 21:09:57 +0300

    добавила задание 16

--- Sem1Task16/Program.cs
+++ Sem1Task16/Program.cs
@@ -1,4 +1,13 @@
 ﻿// Задание  № 16
-Console.Write( "Введите число :" );
-int num = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(num%7==0&&num%23==0? "Кратно":"Не кратно");
+Console.Write( "Введите число A :" );
+int numA = int.Parse(Console.ReadLine()??"0");
+Console.Write( "Введите число B:" );
+int numB = int.Parse(Console.ReadLine()??"0");
+bool test1=(numA==numB*numB);
+bool test2=(numB==numA*numA);
+if(test1)Console.WriteLine("Первое число квадрат второго");
+if(test2)Console.WriteLine("Второе число квадрат первого");
+if (!(test1||test2))
+{
+    Console.WriteLine("Числа неквдраты друг друга");
+}
