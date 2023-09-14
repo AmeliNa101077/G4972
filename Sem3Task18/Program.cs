a12909e34baa4962b419aa0ce54dc11d27d3845f
Natalia <amelina.11@mail.ru>
2023-09-14 23:19:36 +0300

    Добавлена задача 18

---
+++ Sem3Task18/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задание 18
+int QuaterBorderAsk()
+{Console.WriteLine("Введите номер четверти:");
+return  Convert.ToInt32(Console.ReadLine());
+}
+
+//Вводим данные
+string pointsIdent(int numQut)
+{
+    if (numQut==1) return "Возможное значение:x>0,y>0";
+    if (numQut==2) return "Возможное значение:x<0,y>0";
+    if (numQut==3) return "Возможное значение:x<0,y<0";
+    if (numQut==4) return "Возможное значение:x>0,y<0";
+    return "Неверное значение";
+}
+int numQut=QuaterBorderAsk();
+Console.WriteLine (pointsIdent(numQut));
