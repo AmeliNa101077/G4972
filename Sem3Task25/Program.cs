ae9ce592c681d913bdce2d0423ae249349cb63e3
Natalia <amelina.11@mail.ru>
2023-09-18 21:20:04 +0300

    Добавлено задание 25

---
+++ Sem3Task25/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+//(задание со *) Написать калькулятор с операциями +, -, /, * и возведение в степень
+
+
+Console.WriteLine("введите число Num1");
+int Num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число Num2");
+int Num2 = Convert.ToInt32(Console.ReadLine());
+int step = Num1;
+
+for (int i = 1; i < Num2; i++)
+{
+step = step * Num1;
+}
+Console.WriteLine("Num1 в степени Num2 равно: " + step);
