9a690783b89c4fef7b579e83d9fb7844e6ce4a13
Natalia <amelina.11@mail.ru>
2023-09-18 21:21:35 +0300

    Внесены изменения

--- Sem3Task25/Program.cs
+++ Sem3Task25/Program.cs
@@ -3,15 +3,16 @@
 //2, 4 -> 16
 //(задание со *) Написать калькулятор с операциями +, -, /, * и возведение в степень
 
-
+//Введем переменные
 Console.WriteLine("введите число Num1");
 int Num1 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("введите число Num2");
 int Num2 = Convert.ToInt32(Console.ReadLine());
 int step = Num1;
-
+//Проведем расчет
 for (int i = 1; i < Num2; i++)
 {
 step = step * Num1;
 }
+//Выведем результат 
 Console.WriteLine("Num1 в степени Num2 равно: " + step);
