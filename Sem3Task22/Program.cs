3702e60cc10c519dfec401d51ccef13bd723a3e0
Natalia <amelina.11@mail.ru>
2023-09-19 20:33:27 +0300

    добавлено задание 22

---
+++ Sem3Task22/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задание № 22 Напишите программу , принемающую одно число N и выдает таблицу квадратов от 1 до N.
+// Вводим число N
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num= int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+string BuildLine (int n, int Pow)
+{
+    string res=" ";
+    for (int i = 1; i<=n; i++)
+    {
+        res= res + Math.Pow(i,Pow)+" ";
+    }
+    return res;
+}
+   // Печатаем результат
+void PrintResult(string msg)
+{
+   Console.WriteLine (msg);
+}
+int n=ReadData ("Введите число N:");
+
+PrintResult(BuildLine(n,2));
