523200e012eba6a9161b39af5199cf6d1187d197
Natalia <amelina.11@mail.ru>
2023-09-22 22:17:38 +0300

    Добавлено задание со звезд.к зад.24

--- Sem4Task24/Program.cs
+++ Sem4Task24/Program.cs
@@ -16,13 +16,28 @@ void PrintResult(string msg)
 int SimpleSum (int numA)
 {
         int sumOfNamber = 0;
-        for (int i=1;i<numA;i++)
+        for (int i = 1;i<=numA;i++)
         {
              sumOfNamber +=i;      
         }
         return sumOfNamber ;
 }
+int GausseSum (int numA)
+{
+    int sumOfNamber = 0;
+    sumOfNamber=(numA*(numA+1))/2;  
+    return sumOfNamber ;
+}
 int numberA = ReadData ("Введите число А: ");
-int res = SimpleSum (numberA);
 
-PrintResult("Сумма чисел от 1 до " + numberA + " равна " + res );
+    DateTime d1=DateTime.Now;
+    int res1 = SimpleSum(numberA);
+    Console.WriteLine(DateTime.Now-d1);
+
+    DateTime d2=DateTime.Now;
+    int res2=GausseSum(numberA);
+    Console.WriteLine(DateTime.Now-d2);
+
+
+PrintResult("Сумма чисел от 1 до " + numberA + " (простой)равна " + res1 );
+PrintResult("Сумма чисел от 1 до " + numberA + " (Гаусса)равна " + res2 );
