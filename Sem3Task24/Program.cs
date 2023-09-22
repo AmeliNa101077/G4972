1b5bfa590e58d8d703eaabe924e1b70b9f500ed5
Natalia <amelina.11@mail.ru>
2023-09-22 20:55:05 +0300

    добавила задания из теста

--- Sem3Task24/Program.cs
+++ Sem3Task24/Program.cs
@@ -1,5 +1,6 @@
-﻿// Задание № 20
-// 
+﻿// Задание 24 Внутри класса Answer напишите метод DistanceBetweenPoints, который принимает на вход координаты двух точек pointA и pointB в виде массива целых чисел, и возвращает расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7) -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
 using System;
 
 public class Answer
@@ -7,12 +8,10 @@ public class Answer
     private static double Length(int[] pointA, int[] pointB)
     {
       // Введите свое решение ниже
-     double Calc3DDist int x1;int x2;int x3;int y1;int y2;int y3;
+    double Calc3DDist(int x1,int x2,int x3,int y1,int y2,int y3)
     {
     double res = Math.Sqrt((x1-x2-x3)*(x1-x2-x3)+(y1-y2-y3)*(y1-y2-y3));
     return res;
-
-    }
 
     }
 
@@ -42,4 +41,5 @@ public class Answer
         double result = Length(new int[]{x1, x2, x3}, new int[]{y1, y2, y3});
         Console.WriteLine($"{result:F2}");
     }
+}
 }
