e39e84118bd15fc9d29c6de7cd7bc86964df78b3
Natalia <amelina.11@mail.ru>
2023-09-22 21:43:32 +0300

    добавила задание 24 семинар

---
+++ Sem4Task24/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задание 24 из семинара
+// Напишите программу , которая принемает на вход число А и выдает сумму чисел от 1 до А
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num= int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+
+
+// Печатаем результат
+void PrintResult(string msg)
+{
+   Console.WriteLine (msg);
+}
+int SimpleSum (int numA)
+{
+        int sumOfNamber = 0;
+        for (int i=1;i<numA;i++)
+        {
+             sumOfNamber +=i;      
+        }
+        return sumOfNamber ;
+}
+int numberA = ReadData ("Введите число А: ");
+int res = SimpleSum (numberA);
+
+PrintResult("Сумма чисел от 1 до " + numberA + " равна " + res );
