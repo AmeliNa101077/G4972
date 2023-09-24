89c59cf33ecd8fe11703770a68feb1c74de6bd6b
Natalia <amelina.11@mail.ru>
2023-09-24 21:38:52 +0300

    30 зад.надо доделать

---
+++ Sem4Task30/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задание 30
+//
+int ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    int num= int.Parse(Console.ReadLine()??"0");
+    return num;
+}
+// Генерация массива
+int[] GenArr(int len)
+{
+    int[] arr = num int[len];
+    Random rnd = num Random();
+    for (int i=0;i<len;i++)
+    {
+        arr[i]=rnd.Next (0, 2);
+    }
+    return arr;
+}
+
+//Печать массива
+void PrintArr(int[] arr)
+{
+    Console.Write("[");
+    for (int i=0;i < arr.Length-1; i++)
+        Console.Write ( arr[i]+",");   
+}   Console.WritLine ( arr[arr.Length-1] +"]");
+    PrintArry(GenArr);
+
+
+
+
+
+
