d87fbc647338ca624afefcd478238ff1ec641723
Natalia <amelina.11@mail.ru>
2023-09-27 22:18:09 +0300

    Добавлено задание 25 *(степень не считает)

--- Sem3Task25/Program.cs
+++ Sem3Task25/Program.cs
@@ -4,15 +4,86 @@
 //(задание со *) Написать калькулятор с операциями +, -, /, * и возведение в степень
 
 //Введем переменные
-Console.WriteLine("введите число Num1");
-int Num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("введите число Num2");
-int Num2 = Convert.ToInt32(Console.ReadLine());
-int step = Num1;
+//Console.WriteLine("введите число Num1");
+//int Num1 = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("введите число Num2");
+//int Num2 = Convert.ToInt32(Console.ReadLine());
+//int step = Num1;
 //Проведем расчет
-for (int i = 1; i < Num2; i++)
-{
-step = step * Num1;
-}
+//for (int i = 1; i < Num2; i++)
+//{
+//step = step * Num1;
+//}
 //Выведем результат 
-Console.WriteLine("Num1 в степени Num2 равно: " + step);
+//Console.WriteLine("Num1 в степени Num2 равно: " + step);
+
+//Задание со звездочкой
+
+//Вводим данные
+string ReadData(string msg)
+{
+    Console.WriteLine(msg);
+    string data = Console.ReadLine();
+    return data;
+}
+//Функция сложения
+long plus (int a, int b)
+{
+    long result = a + b;
+    return result;
+}
+//Функция вычитания
+long minus (int a, int b)
+{
+    long result = a - b;
+    return result;
+}
+//Функция умножения
+long multiplication (int a, int b)
+{
+    long result = a*b;
+    return result;
+}
+//Функция деления
+long division (int a, int b)
+{
+    long result = a/b;
+    return result;
+}
+//Функция возведения в степень
+long degree (int a, int b)
+{
+    long result = a^b;
+    return result;
+}
+// Печатаем результат 
+void PrintResult (string msg)
+{
+    Console.WriteLine(msg);
+}
+int a =int.Parse(ReadData("Введите число A"));
+int b =int.Parse(ReadData("Введите число B"));
+string operand =ReadData("Введите операцию (+,-,*,/,^)");
+
+long result=0;
+switch(operand)
+{
+    case "+":
+    result=plus(a,b);
+    break;
+    case "-":
+    result=minus(a,b);
+    break;
+    case "*":
+    result=multiplication(a,b);
+    break;
+    case "/":
+    result=division(a,b);
+    break;
+    case  "^":
+    result=degree(a,b);
+    break;
+    default:Console.WriteLine("Что-то пошло не так");
+    break;
+}
+PrintResult("Результат операции="+ result );
