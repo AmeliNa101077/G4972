ed5d7d8ba02c73cb07892efb866873da2335fb38
Natalia <amelina.11@mail.ru>
2023-10-06 22:18:19 +0300

    добавлено задание 51

--- Sem7Task47/Program.cs
+++ Sem7Task47/Program.cs
@@ -7,75 +7,77 @@
 using System;
 
 public class Answer {
-  public static double[, ] CreateRandomMatrix(int m, int n, int minLimitRandom, int maxLimitRandom) {
+    public static void PrintArray (int [,] matrix)
+    {
       // Введите свое решение ниже
-    double[,] arr = new double[m,n];
-        for (int i = 0; i < m; i++)
-            for (int j = 0; j < n; j++)
-                arr[i,j] = (new Random().Next(minLimitRandom, maxLimitRandom)+(new Random().NextDouble()));
-        return arr;
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+  for (int j = 0; j < matrix.GetLength(1); j++)
+  {
+    Console.Write(matrix[i,j]+ "\t");
+  }
+  Console.WriteLine();
+}
 
+    }
+    public static int[,] CreateIncreasingMatrix(int n, int m, int k)
+    {
+      // Введите свое решение ниже
+  int[,] matrix = new int[n,m];
+for (int i = 0; i < n; i++)
+{
+  for (int j = 0; j < m; j++)
+  {
+                if (j == 0 && i == 0) matrix[i,j] = 1;
+                else if (j == 0 && i > 0)  matrix[i, j] = matrix[i - 1, m - 1] + k;
+                else matrix[i, j] = matrix[i, j - 1] + k;
   }
 }
-  public static void PrintArray(double[, ] matrix) {
+return matrix;
+    }
+  
+  
+    public static int[] FindNumberByPosition (int [,] matrix, int rowPosition, int columnPosition)
+    {  
       // Введите свое решение ниже
-    
+   int[] results = new int[2];
+      if(rowPosition - 1 < matrix.GetLength(0) && columnPosition - 1 < matrix.GetLength(1))
+      {
+      results[0] = matrix[rowPosition - 1, columnPosition - 1];
+      return results;
+      }
+      else return results; 
+}
 
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    public static void PrintCheckIfError (int[] results, int X, int Y)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-           
-            {
-                Console.Write(matrix[i,j] + "");
-                
-            }
-                 Console.WriteLine();
-   
-  }
+      // Введите свое решение ниже
+  // Введите свое решение ниже
+if (results[0] == 0) Console.WriteLine("There is no such index");
+else Console.WriteLine($"The number in [{X}, {Y}] is {results[0]}");
     }
-  // Не удаляйте и не меняйте метод Main! 
-  public static void Main(string[] args) {
-    int m, n, minLimitRandom, maxLimitRandom;
+    // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int n, m, k, x, y;
 
-    if (args.Length >= 4) {
-      m = int.Parse(args[0]);
-      n = int.Parse(args[1]);
-      minLimitRandom = int.Parse(args[2]);
-      maxLimitRandom = int.Parse(args[3]);
-
-      double[,] array = CreateRandomMatrix(m, n, minLimitRandom, maxLimitRandom);
-
-        // Выберем случайные индексы трех элементов матрицы array
-        int row1 = new Random().Next(0, m);
-        int col1 = new Random().Next(0, n);
-        int row2 = new Random().Next(0, m);
-        int col2 = new Random().Next(0, n);
-        int row3 = new Random().Next(0, m);
-        int col3 = new Random().Next(0, n);
-
-        // Проверяем, являются ли выбранные элементы дробными числами
-        bool isFractional1 = (array[row1, col1] % 1) != 0;
-        bool isFractional2 = (array[row2, col2] % 1) != 0;
-        bool isFractional3 = (array[row3, col3] % 1) != 0;
-
-        // Если два из трех элементов не являются дробными числами, то бросаем исключение
-        if ((isFractional1 && isFractional2) || (isFractional1 && isFractional3) || (isFractional2 && isFractional3))
-        {
-            Console.WriteLine("Все ок!");
+        if (args.Length >= 5) {
+           n = int.Parse(args[0]);
+           m = int.Parse(args[1]);
+           k = int.Parse(args[2]);
+           x = int.Parse(args[3]);
+           y = int.Parse(args[4]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+           n = 3;
+           m = 4;
+           k = 2;
+           x = 2;
+           y = 3;
         }
-        else
-        {
-            throw new Exception("Выбранные элементы не содержат по крайней мере два дробных числа.");
-        }
-    } else {
-      m = 3;
-      n = 4;
-      minLimitRandom = -10;
-      maxLimitRandom = 10;
-      
-      double[, ] result = CreateRandomMatrix(m, n, minLimitRandom, maxLimitRandom);
-      PrintArray(result);
+
+        // Не удаляйте строки ниже
+        int[,] result = CreateIncreasingMatrix(n, m, k);
+        PrintArray(result);
+        PrintCheckIfError(FindNumberByPosition(result, x, y), x, y);
     }
-  }
 }
